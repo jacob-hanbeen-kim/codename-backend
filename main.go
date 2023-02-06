68c22858382b041174b8035012b25f6296b3ce5e
Daniel Lee <daniellee@daniels-air.lan>
2023-02-06 16:46:09 -0500

    Remove unnecessary files

--- main.go
+++ main.go
@@ -32,45 +32,6 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(databases)
+
 	app.Start()
 }
-
-// func close(client *mongo.Client, ctx context.Context,
-// 	cancel context.CancelFunc) {
-
-// 	defer cancel()
-
-// 	defer func() {
-// 		if err := client.Disconnect(ctx); err != nil {
-// 			panic(err)
-// 		}
-// 	}()
-// }
-
-// This is a user defined method that returns mongo.Client,
-// context.Context, context.CancelFunc and error.
-// mongo.Client will be used for further database operation.
-// context.Context will be used set deadlines for process.
-// context.CancelFunc will be used to cancel context and
-// resource associated with it.
-// func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
-
-// 	ctx, cancel := context.WithTimeout(context.Background(),
-// 		30*time.Second)
-// 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-// 	fmt.Println("Connecting")
-// 	return client, ctx, cancel, err
-// }
-
-// func ping(client *mongo.Client, ctx context.Context) error {
-
-// 	// mongo.Client has Ping to ping mongoDB, deadline of
-// 	// the Ping method will be determined by cxt
-// 	// Ping method return error if any occurred, then
-// 	// the error can be handled.
-// 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-// 		return err
-// 	}
-// 	fmt.Println("connected successfully")
-// 	return nil
-// }
